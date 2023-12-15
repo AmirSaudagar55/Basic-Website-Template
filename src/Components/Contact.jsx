634a5dec6003a59558fcc322555a38befe4e06fd
AmirSaudagar55 <saudagaramir55@gmail.com>
2023-12-15 11:06:01 +0530

    loader and useLoaderData

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Contact() {
+  const data = useLoaderData()
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>Follower : {data.followers}</h1>
     </div>
   )
 }
 
 export default Contact
+
+export const  githubInfoLoader = async ()=>{
+  const response = await fetch ("https://api.github.com/users/AmirSaudagar55")
+  return response.json()
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,37 +2,24 @@ import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Header from './Components/Header.jsx'
 import About from "./Components/About.jsx"
-import Contact from './Components/Contact.jsx'
+import Contact ,{githubInfoLoader} from './Components/Contact.jsx'
 import Services from './Components/Services.jsx'
 
-const router = createBrowserRouter([
-{
-  path : "/",
-  element: <Layout/>,
-  children : [
-    // {
-    //   path: "",
-    //   element: <Header/>
-    // },
-    {
-      path:"/about",
-      element:<About/>
-    },
-    {
-      path:"/contact",
-      element:<Contact/>
-    },
-    {
-      path:"/services",
-      element:<Services/>
-    }
-  ]
-}
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+
+    <Route path='/' element={<Layout/>}>
+        <Route path='/about' element={<About/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/contact' loader={githubInfoLoader} element={<Contact/>} />
+    </Route>
+
+  )
+)
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
